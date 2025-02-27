1c4c114689e8e9869ce0a6e64ae0019acbbf3c84
MASHAGOLOVANOVA <mmgolovanova@mail.ru>
2025-02-28 00:01:16 +0500

    pylint fix

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
 """docstring for run"""
 from bot.bot import bot
-
+import commands
+import auth
+import ed_programme
+import integration
+import meeting
+import menu
+import project
+import student
+import task
 
 print("bot runs")
 bot.polling(none_stop=True, interval=0)
